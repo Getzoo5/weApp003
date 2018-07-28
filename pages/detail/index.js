f610315997c57e2760d1665597fa7f8596bcbb03
Getzoo5 <1780086485@qq.com>
2018-07-28 15:11:59 +0800

    new file 111

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 var data = require('../../utils/data.js')
 const app = getApp()
@@ -34,71 +33,3 @@ exports.default = Page({
     this.setData({ isLook: !this.data.isLook })
   }
 });
-=======
-// pages/detail/index.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
->>>>>>> 53ce11391aeeca4c83184522e9148173e6a5e64c
